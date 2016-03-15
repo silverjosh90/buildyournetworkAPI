84477b23162bd20e5d3eabc6f3480b731821ab0d
Josh Silverstein <silverjosh90@gmail.com>
2016-03-14 21:05:09 -0600

    removed passport stuff

--- routes/auth.js
+++ routes/auth.js
@@ -4,20 +4,4 @@ var passport = require('passport')
 var passportconfig = require('../config/passport')
 var db = require('../data/mongoose')
 
-router.get('/linkedin',
-  passport.authenticate('linkedin'));
-
-router.get('/linkedin/callback',
-  passport.authenticate('linkedin', { failureRedirect: '/login' }),
-  function(req, res) {
-    console.log(req.user)
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
-
-router.get('/logout', function(req,res,next){
-  req.session = null
-  res.redirect('/')
-})
-
 module.exports = router;
