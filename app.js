388ffb153d6b332498051e976e4c53740f5888eb
Josh Silverstein <silverjosh90@gmail.com>
2016-03-14 21:11:42 -0600

    added homepage

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(session({ secret: 'secret secret' }))
 
 app.use('/contacts', contacts);
 app.use('/users', users);
-app.use('/auth', auth);
+app.use('/', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/auth.js
+++ routes/auth.js
@@ -2,4 +2,8 @@ var express = require('express');
 var router = express.Router();
 var db = require('../data/mongoose')
 
+router.get('/', function(req,res){
+  res.send('whats cookin good lookin')
+})
+
 module.exports = router;
