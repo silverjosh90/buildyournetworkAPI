32e8da365dcc8b9c4fa3bb5a1d8114d133c34722
Josh Silverstein <silverjosh90@gmail.com>
2016-03-14 21:06:48 -0600

    fixed passport

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport')
-var passportconfig = require('../config/passport')
 var db = require('../data/mongoose')
 
 module.exports = router;
